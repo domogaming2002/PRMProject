fdcb9b8ff75d42fcd4a094ce20a695655e277691
dung-mai <dungmvhe163263@fpt.edu.vn>
2023-07-19 19:31:18 +0700

    DungMV add DTO

--- app/src/main/java/com/example/projectprm/DTO/ProductDTO.java
+++ app/src/main/java/com/example/projectprm/DTO/ProductDTO.java
@@ -1,12 +1,5 @@
 package com.example.projectprm.DTO;
 
-import androidx.annotation.Nullable;
-import androidx.room.ColumnInfo;
-import androidx.room.PrimaryKey;
-
-import com.example.projectprm.Entity.Category;
-import com.example.projectprm.Entity.Supplier;
-
 public class ProductDTO { private int productId;
     private String name;
     private int unitInStock;
@@ -15,13 +8,135 @@ public class ProductDTO { private int productId;
     private double rating;
     private String description;
     private int categoryId;
-    private Category category;
+    private CategoryDTO category;
     private int supplyId;
-    private Supplier supply;
+    private SupplierDTO supply;
     private boolean isDelete  = false;
 
     public ProductDTO() {
     }
 
+    public int getProductId() {
+        return productId;
+    }
 
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getUnitInStock() {
+        return unitInStock;
+    }
+
+    public void setUnitInStock(int unitInStock) {
+        this.unitInStock = unitInStock;
+    }
+
+    public double getSalePrice() {
+        return salePrice;
+    }
+
+    public void setSalePrice(double salePrice) {
+        this.salePrice = salePrice;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public CategoryDTO getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoryDTO category) {
+        this.category = category;
+    }
+
+    public int getSupplyId() {
+        return supplyId;
+    }
+
+    public void setSupplyId(int supplyId) {
+        this.supplyId = supplyId;
+    }
+
+    public SupplierDTO getSupply() {
+        return supply;
+    }
+
+    public void setSupply(SupplierDTO supply) {
+        this.supply = supply;
+    }
+
+    public boolean isDelete() {
+        return isDelete;
+    }
+
+    public void setDelete(boolean delete) {
+        isDelete = delete;
+    }
+
+    public ProductDTO(int productId, String name, int unitInStock, double salePrice, String image, double rating, String description, int categoryId, CategoryDTO category, int supplyId, SupplierDTO supply, boolean isDelete) {
+        this.productId = productId;
+        this.name = name;
+        this.unitInStock = unitInStock;
+        this.salePrice = salePrice;
+        this.image = image;
+        this.rating = rating;
+        this.description = description;
+        this.categoryId = categoryId;
+        this.category = category;
+        this.supplyId = supplyId;
+        this.supply = supply;
+        this.isDelete = isDelete;
+    }
+
+    public ProductDTO(int productId, String name, int unitInStock, double salePrice, String image, double rating, String description, int categoryId, int supplyId, boolean isDelete) {
+        this.productId = productId;
+        this.name = name;
+        this.unitInStock = unitInStock;
+        this.salePrice = salePrice;
+        this.image = image;
+        this.rating = rating;
+        this.description = description;
+        this.categoryId = categoryId;
+        this.supplyId = supplyId;
+        this.isDelete = isDelete;
+    }
 }
--- app/src/main/java/com/example/projectprm/DTO/SupplierDTO.java
+++ app/src/main/java/com/example/projectprm/DTO/SupplierDTO.java
@@ -1,4 +1,73 @@
 package com.example.projectprm.DTO;
 
-public class SupplierDTO {
+import androidx.room.ColumnInfo;
+import androidx.room.PrimaryKey;
+
+public class SupplierDTO { private int supplyId;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+    private boolean isDelete  = false;
+
+    public SupplierDTO(int supplyId, String name, String email, String phone, String address, boolean isDelete) {
+        this.supplyId = supplyId;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.isDelete = isDelete;
+    }
+
+    public SupplierDTO() {
+
+    }
+
+    public int getSupplyId() {
+        return supplyId;
+    }
+
+    public void setSupplyId(int supplyId) {
+        this.supplyId = supplyId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public boolean isDelete() {
+        return isDelete;
+    }
+
+    public void setDelete(boolean delete) {
+        isDelete = delete;
+    }
 }
